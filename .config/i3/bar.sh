321effcebba019df51ec23878efd4ff1261336f9
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-09-29 17:51:16 +0300

    Battery indicator in bar, hardware optimization and more

--- .config/i3/bar.sh
+++ .config/i3/bar.sh
@@ -2,6 +2,6 @@ while :
 #IFS=$(echo -en "\t\n\0")
 
 do
-	echo $(xkblayout-state print %s) " "  $(date "+%H:%M:%S %d/%m")
-	sleep 0.1 
+	echo $(xkblayout-state print %s) " " $(acpi -b | cut -d  "," -f 2) " " $(date "+%H:%M")
+	sleep 5 
 done
