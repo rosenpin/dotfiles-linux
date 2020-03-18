5f6f92a0b5bbe73e334d26d6a9fa16180daad6d3
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2020-03-18 20:22:34 +0200

    fixed switch output script to support more than 2 sinks

--- scripts/.scripts/switch_output.sh
+++ scripts/.scripts/switch_output.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-sinks=($(pacmd list-sinks | grep index | \
-    awk '{ if ($1 == "*") print "1",$3; else print "0",$2 }'))
+sinks="$(pacmd list-sinks | grep index | awk '{ if ($1 == "*") print "1",$3; else print "0",$2 }')"
+#sinks=($(pacmd list-sinks | grep index | \
+#    awk '{ if ($1 == "*") print "1",$3; else print "0",$2 }'))
 inputs=($(pacmd list-sink-inputs | grep index | awk '{print $2}'))
 
-[[ ${sinks[0]} = 0 ]] && swap=${sinks[1]} || swap=${sinks[3]}
+#[[ ${sinks[0]} = 0 ]] && swap=${sinks[1]} || swap=${sinks[3]}
+temp="$(echo "$sinks" | grep ^1 -A1)"
+if [[ "$temp" == $(echo "$temp" | tail -n1) ]]; then
+    echo "same, going to first"
+    swap=$(echo $sinks | head -n1)
+else
+    echo "going to next"
+    swap=$(echo "$temp" | tail -n1)
+fi
+swap=$(echo "$swap" | awk '{print $2}')
+echo "$swap"
 
 pacmd set-default-sink $swap &> /dev/null
 for i in ${inputs[*]}; do pacmd move-sink-input $i $swap &> /dev/null; done
